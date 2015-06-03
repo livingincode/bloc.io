3d24394bcaae1af95b85b342c124c6ec8f95b9b5
Dean Laurea <deanlaurea@gmail.com>
2015-06-02 20:31:14 -0500

    first commit

---
+++ Car/Car/Car.h
@@ -0,0 +1,13 @@
+//
+//  Car.h
+//  Car
+//
+//  Created by Dean Laurea on 5/22/15.
+//  Copyright (c) 2015 Dean Laurea. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Car : NSObject
+
+@end
---
+++ Car/Car/Car.m
@@ -0,0 +1,13 @@
+//
+//  Car.m
+//  Car
+//
+//  Created by Dean Laurea on 5/22/15.
+//  Copyright (c) 2015 Dean Laurea. All rights reserved.
+//
+
+#import "Car.h"
+
+@implementation Car
+
+@end
